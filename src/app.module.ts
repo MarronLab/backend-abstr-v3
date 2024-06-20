9d6f492cf4c165670ec4d439b10c4cc34734678b
Sadikdeveloper <sadikdeveloper3215@gmail.com>
2024-06-20 02:36:25 +0100

    merged

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { OrderModule } from './modules/orderBook/order.module';
 import { AuthModule } from './modules/auth/module/auth.module';
 import { UserModule } from './modules/user/module/user.module';
 import { MarketModule } from './modules/market/module/market.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
   imports: [OrderModule, AuthModule, UserModule, MarketModule],
   controllers: [],
   providers: [],
+  exports: [],
 })
 export class AppModule {}
