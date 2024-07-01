6be8e2f37845654239ec671fc7520862072be214
Sadikdeveloper <sadikdeveloper3215@gmail.com>
2024-07-01 10:32:51 +0100

    base url fixed

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -43,7 +43,7 @@ export class AuthService {
         mobileOTP: registerDto.mobileOTP,
       });
 
-      console.log(data);
+      // console.log(data);
       if (data.status === 'Error') {
         throw new UnprocessableEntityException(data.data);
       }
