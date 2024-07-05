a5b89d445a571abd7b66989c1fd0014cd2aa09a3
Sadikdeveloper <sadikdeveloper3215@gmail.com>
2024-07-05 16:49:30 +0100

    added trending dto

--- src/modules/market/dto/market.dto.ts
+++ src/modules/market/dto/market.dto.ts
@@ -121,3 +121,76 @@ export class MarketDataResponseDto {
     Object.assign(this, partial);
   }
 }
+
+class PriceChangePercentage24hDto {
+  @ApiProperty()
+  btc: number;
+
+  @ApiProperty()
+  usd: number;
+}
+
+class DataDto {
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  price_btc: number;
+
+  @ApiProperty({ type: PriceChangePercentage24hDto })
+  price_change_percentage_24h: PriceChangePercentage24hDto;
+
+  @ApiProperty()
+  market_cap: string;
+
+  @ApiProperty()
+  market_cap_btc: string;
+
+  @ApiProperty()
+  total_volume: string;
+
+  @ApiProperty()
+  total_volume_btc: string;
+
+  @ApiProperty()
+  sparkline: string;
+}
+
+export class TrendingMarketDataResponseDto {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  coin_id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  symbol: string;
+
+  @ApiProperty()
+  market_cap_rank: number;
+
+  @ApiProperty()
+  thumb: string;
+
+  @ApiProperty()
+  small: string;
+
+  @ApiProperty()
+  large: string;
+
+  @ApiProperty()
+  price_btc: string;
+
+  @ApiProperty()
+  score: number;
+
+  @ApiProperty({ type: DataDto })
+  data: DataDto;
+
+  constructor(partial: Partial<TrendingMarketDataResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
