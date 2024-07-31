9e691ebe7a7e49d2a3bfa89eda8154933d67203a
Idongesit Ntuk <ideeofficial@gmail.com>
2024-07-31 13:25:57 +0100

    fix

--- src/services/modulus/modulus.service.ts
+++ src/services/modulus/modulus.service.ts
@@ -294,7 +294,7 @@ export class ModulusService {
 
   async validateBearerToken() {
     return await this.post<ValidateBearerTokenResponse>(
-      `/Validate_BearerToken`,
+      `/api/Validate_BearerToken`,
     );
   }
 
