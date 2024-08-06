b4333bdf9117158b4dd2319ccf455bc78030b62a
Idongesit Ntuk <ideeofficial@gmail.com>
2024-08-06 03:28:12 +0100

    add more details

--- src/modules/orderBook/dto/orderHistory.dto.ts
+++ src/modules/orderBook/dto/orderHistory.dto.ts
@@ -3,20 +3,36 @@ import { IsDefined, IsEnum, IsNotEmpty } from 'class-validator';
 import { OrderSideExtendedEnum } from 'src/services/modulus/modulus.enum';
 
 export class OrderHistoryDto {
-  @ApiProperty({ enum: OrderSideExtendedEnum })
+  @ApiProperty({
+    enum: OrderSideExtendedEnum,
+    description:
+      'Side of the order, indicating whether it is a BUY or SELL. Extended options may include both sides.',
+    example: OrderSideExtendedEnum.ALL,
+  })
   @IsDefined()
   @IsNotEmpty()
   @IsEnum(OrderSideExtendedEnum)
   side: OrderSideExtendedEnum;
 
-  @ApiProperty({ example: 'BTC-ETH' })
+  @ApiProperty({
+    description:
+      'Trading pair for the order history (e.g., BTC-ETH). Also accepts `ALL`',
+    example: 'BTC-ETH',
+  })
   @IsDefined()
   @IsNotEmpty()
   pair: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description:
+      'Number of records to retrieve. Defaults to a predefined number if not provided.',
+    example: 10,
+  })
   count?: number;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'Page number to retrieve in a paginated result set.',
+    example: 1,
+  })
   page?: number;
 }
--- src/modules/orderBook/dto/tradeHistory.dto.ts
+++ src/modules/orderBook/dto/tradeHistory.dto.ts
@@ -3,20 +3,37 @@ import { IsDefined, IsEnum, IsNotEmpty } from 'class-validator';
 import { OrderSideExtendedEnum } from 'src/services/modulus/modulus.enum';
 
 export class TradeHistoryDto {
-  @ApiProperty({ enum: OrderSideExtendedEnum })
+  @ApiProperty({
+    enum: OrderSideExtendedEnum,
+    description:
+      'Side of the trade, indicating whether it is a BUY or SELL. Extended options may include both sides.',
+    example: OrderSideExtendedEnum.ALL,
+  })
   @IsDefined()
   @IsNotEmpty()
   @IsEnum(OrderSideExtendedEnum)
   side: OrderSideExtendedEnum;
 
-  @ApiProperty({ example: 'BTC-ETH' })
+  @ApiProperty({
+    example: 'BTC-ETH',
+    description:
+      'Trading pair for the trade history (e.g., BTC-ETH). Also accepts `ALL`',
+  })
   @IsDefined()
   @IsNotEmpty()
   pair: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description:
+      'Number of records to retrieve. Defaults to a predefined number if not provided.',
+    example: 10,
+  })
   count?: number;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description:
+      'Page number to retrieve in a paginated result set. Defaults to 1',
+    example: 1,
+  })
   page?: number;
 }
