a41bd9ca45bd485c5adff440151a163b813f1ace
Idongesit Ntuk <ideeofficial@gmail.com>
2024-08-12 14:11:17 +0100

    Remove console logs

--- src/services/modulus/modulus.service.ts
+++ src/services/modulus/modulus.service.ts
@@ -98,13 +98,9 @@ export class ModulusService {
         request,
         config,
       );
-      // TODO: remove later - keep to trace token request error
-      console.log('response', response);
 
       return response;
     } catch (error) {
-      // TODO: remove later - keep to trace token request error
-      console.log('error', error);
       this.handleError(error);
     }
   }
