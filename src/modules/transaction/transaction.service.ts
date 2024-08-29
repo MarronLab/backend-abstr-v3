88a04e78af7e39d323dcd941663652ef02179ca1
Idongesit Ntuk <ideeofficial@gmail.com>
2024-08-29 09:00:07 +0100

    refactor

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,16 +1,30 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { GetAllTransactionsDto } from './dto/transaction.dto';
 import { MoralisService } from 'src/services/moralis/moralis.service';
 import HelperProvider from 'src/utils/helperProvider';
+import { SettingsService } from '../settings/settings.service';
 
 @Injectable()
 export class TransactionService {
-  constructor(private readonly moralisService: MoralisService) {}
+  constructor(
+    private readonly moralisService: MoralisService,
+    private readonly settingsService: SettingsService,
+  ) {}
   async getAllTransactions(
     address: string,
     getAllTransactionsDto: GetAllTransactionsDto,
   ) {
     try {
+      const data = await this.settingsService.getApiSettings();
+
+      if (!data.supportedAssets) {
+        throw new UnprocessableEntityException();
+      }
+
       const response = await this.moralisService.transactions({
         cursor: getAllTransactionsDto.cursor,
         limit: getAllTransactionsDto.limit,
@@ -22,7 +36,26 @@ export class TransactionService {
         response.result,
       );
 
-      return { ...response, result };
+      const transactions = data.supportedAssets
+        .map((asset) => {
+          const transaction = result.find(
+            (x) =>
+              x.token_symbol.toLowerCase() === asset!.shortName.toLowerCase(),
+          );
+
+          return transaction;
+        })
+        .filter((transaction) => transaction !== undefined) as {
+        hash: string;
+        block_timestamp: string;
+        receipt_status: string;
+        token_symbol: string;
+        value: number;
+        direction: string;
+        type: string;
+      }[];
+
+      return { ...response, result: transactions };
     } catch (error) {
       console.log('ERROR: ', error);
       throw new InternalServerErrorException(error);
