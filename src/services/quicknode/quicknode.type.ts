cebc49616c80e10ed55a4bf3b79f3877e223a6df
Peter Munachi <alimunachipeter@gmail.com>
2024-10-30 10:45:47 +0100

    feat: indentation fixes

--- src/services/quicknode/quicknode.type.ts
+++ src/services/quicknode/quicknode.type.ts
@@ -5,20 +5,20 @@ interface MultiTokenValue {
 
 export enum TokenType {
   ERC20 = "ERC-20",
-	ERC721 = "ERC-721",
-	ERC1155 = "ERC-1155",
-	OTHER = "Other" // Use "Other" to cover any unexpected types
+  ERC721 = "ERC-721",
+  ERC1155 = "ERC-1155",
+  OTHER = "Other" // Use "Other" to cover any unexpected types
 }
 
 export interface TokenInfo {
-	type: TokenType; // Enum for token type (ERC-20, ERC-721, or ERC-1155)
-	name: string;
-	contract: string;
-	transfers: number;
-	symbol: string;
-	decimals: number;
-	multiTokenValues?: MultiTokenValue[];
-	balance?: string;
+  type: TokenType; // Enum for token type (ERC-20, ERC-721, or ERC-1155)
+  name: string;
+  contract: string;
+  transfers: number;
+  symbol: string;
+  decimals: number;
+  multiTokenValues?: MultiTokenValue[];
+  balance?: string;
 }
 
 interface AddressResult {
@@ -45,7 +45,7 @@ export interface AddressBalanceResponse {
 export interface ExchangeRatesResponse {
   jsonrpc: string;
   result: {
-	rates: Record<string, number>; // Exchange rates with currency codes as keys and rates as values
+    rates: Record<string, number>; // Exchange rates with currency codes as keys and rates as values
     fetched_at: string;
   };
   id: number;
